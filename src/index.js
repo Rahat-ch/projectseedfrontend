5de14f6c8c95a4a4e26f366d12d293ef7f1ba936
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-12-22 23:34:24 -0500

    initial commit with reach and testing template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Router, Link } from "@reach/router";
+import Home from "./pages/home";
+import Main from "./pages/main";
+
+const App = ({ children }) => (
+  <Router>
+    <Home path="/" />
+    <Main path="/main" />
+  </Router>
+);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
---
+++ src/pages/home/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Home = () => (
+  <h1>welcome to home page</h1>
+);
+
+export default Home;
